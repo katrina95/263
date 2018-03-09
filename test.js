840226bbd71c831e8fce68279b79446157c913cb
katrina95 <34797724+katrina95@users.noreply.github.com>
2018-03-08 20:40:11 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 can anyone edit or see this? Try to edit it, commit and push
+ 
+answer for q5:
+A 6
+B 1
+C 2
+D
+1.["month","beijing","delhi","fresno"]
+2.
+e
+1.["11","12'..."15","11"]
+  2. 
+   3. array of string
